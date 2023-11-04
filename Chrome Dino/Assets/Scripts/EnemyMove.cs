e0de7526954a4dd913d7e285532fed0eefd25d48
JDY924 <110305269+JDY924@users.noreply.github.com>
2023-11-03 22:47:54 -0700

    14-21 minutes
    completed tutorial to 21:15

---
+++ Chrome Dino/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        
+    }
+ 
+    void Update()
+    {
+        transform.Translate(Vector2.left*Time.deltaTime*speed);
+    }
+}
--- Chrome Dino/Assets/Scripts/Player.cs
+++ Chrome Dino/Assets/Scripts/Player.cs
@@ -17,19 +17,24 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0)&&isJumping==true)
         {
             rb.AddForce(Vector2.up*Jump);
-            isJumping = true;
+            isJumping = false;
             anim.SetBool("IsJumping", true);
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ground")
+        if (collision.gameObject.tag == "Ground"&&isJumping==false)
         {
-            isJumping |= false;
+            isJumping = true;
             anim.SetBool("IsJumping", false);
+        }
+        if (collision.gameObject.tag=="Enemy")
+        {
+            anim.SetBool("Dead",true);
+            Time.timeScale=0;
         }
     }
 }
